140614a085f0ac2c76956d50c816382a03a4e751
Sameer Srivastava <sameer13@gmail.com>
2022-02-21 17:59:14 +0530

    Project setup (#6)
    * chore: basic setup
    
    * chore: more setup
    
    * chore: packages updated, test added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const core = require('@actions/core')
+const run = require('./action.js')
+
+run().catch(error => core.setFailed(error))
---
+++ test/log.test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const tap = require('tap')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+const core = require('@actions/core')
+
+const setup = () => {
+  const coreStub = sinon.stub(core)
+  const logger = proxyquire('../src/log', {
+    '@actions/core': coreStub
+  })
+
+  return { coreStub, logger }
+}
+
+tap.afterEach(() => {
+  sinon.restore()
+})
+
+tap.test('calling log with an array will stringify it', async () => {
+  const { logger, coreStub } = setup()
+  logger.logDebug([1, 2, 3])
+  sinon.assert.calledWithExactly(coreStub.debug, '1,2,3')
+})
+
+tap.test('logDebug calls @actions/core/debug', async () => {
+  const { logger, coreStub } = setup()
+  logger.logDebug('Debug')
+  sinon.assert.calledOnce(coreStub.debug)
+  sinon.assert.notCalled(coreStub.error)
+})
+
+tap.test('logError calls @actions/core/error', async () => {
+  const { logger, coreStub } = setup()
+  logger.logError(new Error('not a string'))
+  sinon.assert.calledOnce(coreStub.error)
+  sinon.assert.notCalled(coreStub.debug)
+})
+
+tap.test('logInfo calls @actions/core/info', async () => {
+  const { logger, coreStub } = setup()
+  logger.logInfo('Debug')
+
+  sinon.assert.calledOnce(coreStub.info)
+  sinon.assert.notCalled(coreStub.debug)
+})
+
+tap.test('logWarning calls @actions/core/warning', async () => {
+  const { logger, coreStub } = setup()
+  logger.logWarning('warning')
+
+  sinon.assert.calledOnce(coreStub.warning)
+  sinon.assert.notCalled(coreStub.debug)
+})
