1f57623f142213d51d1ff61fcfc5d44a6c1252f8
Ramon Mulia <ramonmulia@gmail.com>
2022-03-10 11:02:17 -0300

    fix

--- src/action.js
+++ src/action.js
@@ -23,7 +23,7 @@ async function run() {
       core.getInput('project-number', { required: true }) &&
       Number(core.getInput('project-number'))
     const columnName = core.getInput('column-name')
-    const login = context.payload.organization.login
+    const login = github.context.payload.organization.login
 
     const isProjectBeta = await checkIsProjectBeta(login, projectNumber)
 
